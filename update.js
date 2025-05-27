2fb4cef38b255f170d9abe8ddbc7ac3d3d19b16f
GuardianOwO <ydesigndoc@gmail.com>
2025-05-27 17:20:11 +0900

    Update update.js

--- update.js
+++ update.js
@@ -4,8 +4,13 @@
             "updates": [
                 // 新しいバージョンをリリースする際は、ここに新しいバージョンのエントリを先頭に追加
                 {
+                    "version": "2.0.1", // ★現在のバージョン番号
+                    "update_link": "https://guardianowo.github.io/DIM_for_Firefox/orenodim-2.0.1.xpi", // ★後で変更
+                    "update_hash": "sha256:68277700B5C38960C64CDD13E6344E84937F46BA342B24AA95E32F07A1C2A08B"
+                },
+                {
                     "version": "2.0", // ★現在のバージョン番号
-                    "update_link": "https://YOUR_GITHUB_USERNAME.github.io/YOUR_REPOSITORY_NAME/my_extension-1.0.0.xpi", // ★後で変更
+                    "update_link": "https://guardianowo.github.io/DIM_for_Firefox/orenodim-2.0.xpi", // ★後で変更
                     "update_hash": "sha256:542D974820702E51FCBFB8841E64019534EFE6F69AF74FCCB27EE352EF3148DB"
                 }
             ]
