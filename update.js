a8e1c5185e917f59d9215f101525d0de79668352
GuardianOwO <ydesigndoc@gmail.com>
2025-05-27 17:11:30 +0900

    Add files via upload

---
+++ update.js
@@ -0,0 +1,14 @@
+{
+    "addons": {
+        "{b0713893-2911-5e35-31e9-870d10186753}": {  // ★ここをあなたの拡張機能のIDに置き換える
+            "updates": [
+                // 新しいバージョンをリリースする際は、ここに新しいバージョンのエントリを先頭に追加
+                {
+                    "version": "2.0", // ★現在のバージョン番号
+                    "update_link": "https://YOUR_GITHUB_USERNAME.github.io/YOUR_REPOSITORY_NAME/my_extension-1.0.0.xpi", // ★後で変更
+                    "update_hash": "sha256:542D974820702E51FCBFB8841E64019534EFE6F69AF74FCCB27EE352EF3148DB"
+                }
+            ]
+        }
+    }
+}
